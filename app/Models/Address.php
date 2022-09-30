88ffd999da8aa4de2ca1e2718aaeec00a3a9178b
Cesar A. Pires <cesar.augusto.pires@hotmail.com>
2022-09-30 10:52:52 -0300

    feat: add Address (migrate, model, factory, seeder), and change Database Seeder (add call factory)

---
+++ app/Models/Address.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'cep',
+        'address',
+        'complement',
+        'number',
+        'district',
+        'city',
+        'state',
+        'country',
+        'ibge',
+        'gia',
+        'ddd',
+        'siafi',
+    ];
+}
