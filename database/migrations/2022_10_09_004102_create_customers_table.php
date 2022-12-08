91b97e24f516c89970599427569cb3318b4ee7f2
Cesar Pires <cesar.augusto.pires@hotmail.com>
2022-12-08 16:46:03 -0300

    fix: convert birthdate datatable to nullable

--- database/migrations/2022_10_09_004102_create_customers_table.php
+++ database/migrations/2022_10_09_004102_create_customers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->date('birthdate');
+            $table->date('birthdate')->nullable();
             $table->string('cpf', 14)->nullable();
             $table->string('rg', 14)->nullable();
             $table->string('cellphone', 16)->nullable();
