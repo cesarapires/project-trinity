8f088ff5262690ade6b5e976083b3bea83a4b4df
Cesar Pires <cesar.augusto.pires@hotmail.com>
2022-12-12 20:50:15 -0300

    doc: add documentation of order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -25,21 +25,194 @@ class OrderController extends Controller
         $this->orderService = $orderService;
     }
 
+    /**
+     * @OA\Post(
+     *     tags={"Order"},
+     *     path="/api/v1/orders",
+     *     description="Store new order",
+     *     @OA\RequestBody(
+     *          @OA\MediaType(
+     *              mediaType="application/x-www-form-urlencoded", 
+     *              @OA\Schema(
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="date_order",
+     *                      ref="#/components/schemas/Order/properties/date_order",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="status",
+     *                      ref="#/components/schemas/Order/properties/status",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="subtotal_products",          
+     *                      ref="#/components/schemas/Order/properties/subtotal_products",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="discount",
+     *                      ref="#/components/schemas/Order/properties/discount",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="addition",
+     *                      ref="#/components/schemas/Order/properties/addition",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="total",
+     *                      ref="#/components/schemas/Order/properties/total",
+     *                  ),
+     *              ),
+     *          ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="The order was created successfully!",
+     *         @OA\JsonContent(
+     *            @OA\Property(property="code", type="string", example="success_created"),
+     *            @OA\Property(property="message", type="string", example="The order was created successfully!")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="417",
+     *         description="Could not create a new order!",
+     *         @OA\JsonContent(
+     *            @OA\Property(property="code", type="string", example="failed_created"),
+     *            @OA\Property(property="message", type="string", example="Could not create a new order!")
+     *         ),
+     *     )
+     * )
+     */
     public function store(StoreRequest $request)
     {
         return $this->orderService->getCreateService()->create($request);
     }
 
+    /**
+     * @OA\Get(
+     *     tags={"Order"},
+     *     path="/api/v1/orders/{order_id}",
+     *     description="Get the order",
+     *     @OA\Parameter(
+     *         name="order_id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(ref="#/components/schemas/Order/properties/id")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Success: Found Order.",
+     *         @OA\JsonContent(ref="#/components/schemas/Order"),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Error: Not Found",
+     *         @OA\JsonContent(
+     *            @OA\Property(property="code", type="string", example="not_found"),
+     *            @OA\Property(property="message", type="string", example="Order {order_id} not found!")
+     *         ),
+     *     )
+     * )
+     */
     public function show($order_id)
     {
         return $this->orderService->getShowService()->show($order_id);
     }
 
+    /**
+     * @OA\Put(
+     *     tags={"Order"},
+     *     path="/api/v1/orders",
+     *     description="Update order",
+     *     tags={"Order"},
+     *     path="/api/v1/orders/{order_id}",
+     *     @OA\Parameter(
+     *         name="order_id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(ref="#/components/schemas/Order/properties/id")
+     *     ),
+     *     @OA\RequestBody(
+     *          @OA\MediaType(
+     *              mediaType="application/x-www-form-urlencoded", 
+     *              @OA\Schema(
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="date_order",
+     *                      ref="#/components/schemas/Order/properties/date_order",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="status",
+     *                      ref="#/components/schemas/Order/properties/status",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="subtotal_products",          
+     *                      ref="#/components/schemas/Order/properties/subtotal_products",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="discount",
+     *                      ref="#/components/schemas/Order/properties/discount",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="addition",
+     *                      ref="#/components/schemas/Order/properties/addition",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="total",
+     *                      ref="#/components/schemas/Order/properties/total",
+     *                  ),
+     *              ),
+     *          ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="The order has been updated successfully!",
+     *         @OA\JsonContent(
+     *            @OA\Property(property="code", type="string", example="success_updated"),
+     *            @OA\Property(property="message", type="string", example="The order has been updated successfully!")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="417",
+     *         description="Could not update a order!",
+     *         @OA\JsonContent(
+     *            @OA\Property(property="code", type="string", example="failed_updated"),
+     *            @OA\Property(property="message", type="string", example="Could not update a order!")
+     *         ),
+     *     )
+     * )
+     */
     public function update(UpdateRequest $request, $order_id)
     {
         return $this->orderService->getUpdateService()->update($order_id, $request);
     }
-
+    
+    /**
+     * @OA\Delete(
+     *     tags={"Order"},
+     *     path="/api/v1/orders/{order_id}",
+     *     description="Delete the order",
+     *     @OA\Parameter(
+     *         name="order_id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(ref="#/components/schemas/Order/properties/id")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="The order has been deleted successfully!",
+     *         @OA\JsonContent(
+     *            @OA\Property(property="code", type="string", example="success_deleted"),
+     *            @OA\Property(property="message", type="string", example="The order has been deleted successfully!")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Could not delete a order!",
+     *         @OA\JsonContent(
+     *            @OA\Property(property="code", type="string", example="failed_deleted"),
+     *            @OA\Property(property="message", type="string", example="Could not delete a order!")
+     *         ),
+     *     )
+     * )
+     */
     public function destroy(Order $order_id)
     {
         return $this->orderService->getDeleteService()->delete($order_id);
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,9 +5,79 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     description="Order model",
+ *     type="object",
+ *     title="Order model"
+ * )
+ */
 class Order extends Model
 {
     use HasFactory;
+
+    /**
+     * The unique identifier of a order.
+     *
+     * @var int
+     *
+     * @OA\Property(format="int64", example=1)
+     */
+    private $id;
+
+    /**
+     * The date of order.
+     *
+     * @var string
+     *
+     * @OA\Property(format="date", example="2002-12-08")
+     */
+    private $date_order;
+
+    /**
+     * The status of order.
+     *
+     * @var string
+     *
+     * @OA\Property(example="O",maxLength=14)
+     */
+    private $status;
+
+    /**
+     * The status of order.
+     *
+     * @var float
+     *
+     * @OA\Property(example="14.56",maxLength=14)
+     */
+    private $subtotal_products;
+
+    /**
+     * The discount of order.
+     *
+     * @var float
+     *
+     * @OA\Property(example="1",maxLength=14)
+     */
+    private $discount;
+
+    /**
+     * The addition of order.
+     *
+     * @var float
+     *
+     * @OA\Property(example="5.46",maxLength=14)
+     */
+    private $addition;
+
+    /**
+     * The total of order.
+     *
+     * @var float
+     *
+     * @OA\Property(example="19.00",maxLength=14)
+     */
+    private $total;
 
     protected $fillable = [
         'date_order',
